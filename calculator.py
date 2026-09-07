5578c494be5011f0ba747688cfc2a5839bc05fb9
Rohan Jais <rohanjaisek@gmail.com>
2026-09-07 17:25:10 +0530

    Added calculator program

---
+++ calculator.py
@@ -0,0 +1,28 @@
+#simple Calculator
+print("*-*"*20)
+print("Simple Calculator")
+print("*-*"*20)
+while True:
+    print()
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print()
+    choice=int(input("Enter your choice(1/2/3/4): "))
+    print()
+    if choice in (1,2,3,4):
+        num1=float(input("Enter first number:"))
+        num2=float(input("Enter second number:"))
+        print()
+        if choice==1:
+            print(num1,"+",num2,"=",num1+num2)
+        elif choice==2:
+            print(num1,"-",num2,"=",num1-num2)      
+        elif choice==3:
+            print(num1,"*",num2,"=",num1*num2)          
+        elif choice==4:
+            if num2==0:
+                print("Enter division by zero not possible")
+            else:
+                print(num1,"/",num2,"=",num1/num2)
